c179e94848e25a8d8cd61ca0875a7596a301245e
Marc553 <marcramis2016@gmail.com>
2022-10-30 18:18:57 +0100

    7

--- Assets/Scripts/Exercise_7.cs
+++ Assets/Scripts/Exercise_7.cs
@@ -6,5 +6,15 @@ using TMPro;
 using UnityEngine.SceneManagement;
 
 public class Exercise_7 : MonoBehaviour
-{ 
+{
+    public GameObject sphere;
+
+    public void ChangeAlpha(float alphaVal)
+    {   
+        Color oldColor = sphere.GetComponent<Renderer>().material.color;
+        oldColor.a = alphaVal;
+        sphere.GetComponent<Renderer>().material.color = oldColor;
+        
+    }
+
 }
